74161f53f7f52fff5f77863a1905b3103766f085
sneguroma <sneguroma@gmail.com>
2023-06-15 13:26:47 +0200

    feat: add copy and create func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { cp } from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+  const dirPath = fileURLToPath(new URL(".", import.meta.url));
+  const files = join(dirPath, "files");
+  const files_copy = join(dirPath, "files_copy");
+
+  function callback(err) {
+    if (err) throw new Error("FS operation failed");
+    console.log("files was copied to files_copy");
+    }
+    
+  cp(
+    files,
+    files_copy,
+    {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    },
+    callback
+  );
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile, access, constants } from "node:fs";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Buffer } from "node:buffer";
+
 const create = async () => {
-    // Write your code here 
+  const data = new Uint8Array(Buffer.from("I am fresh and young"));
+  const dirPath = fileURLToPath(new URL(".", import.meta.url));
+  //const pathToFolder = dirname(fileURLToPath(import.meta.url));
+  const file = join(dirPath, "files", "fresh.txt");
+  writeFile(file, data, { flag: "wx" }, (err) => {
+    if (err) throw new Error("FS operation failed");
+    console.log("Data has been writed!");
+  });
 };
 
 await create();
