03ceffd5b34950c9836035edc20b8979a296ccb7
sneguroma <sneguroma@gmail.com>
2023-06-16 19:31:27 +0200

    feat: add cli args func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const arr = process.argv
+    .slice(2)
+    .reduce((acc, item, index) => {
+      if (index % 2 === 0) return acc + ", " + item.slice(2) + " is ";
+      else return acc + item;
+    }, "")
+    .slice(2);
+  console.log(arr);
 };
 
 parseArgs();
