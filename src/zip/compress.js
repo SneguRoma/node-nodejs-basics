6d9f8540766878fddd7b63130a0eb11e8d108e94
sneguroma <sneguroma@gmail.com>
2023-06-19 16:17:21 +0200

    feat: add zlib  funcs

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from 'node:stream';
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const compress = async () => {
-    // Write your code here 
+  const dirPath = fileURLToPath(new URL(".", import.meta.url));
+  const fileWright = join(dirPath, "files", "archive.gz");
+  const fileRead = join(dirPath, "files", "fileToCompress.txt");
+  const gzip = createGzip();
+  const source = createReadStream(fileRead);
+  const destination = createWriteStream(fileWright);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("FS operation failed:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
