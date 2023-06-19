aa7e27ff93dcb508389443edc30cb9c24c151035
sneguroma <sneguroma@gmail.com>
2023-06-19 15:08:28 +0200

    feat: add steams  funcs

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { stdout } from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  const dirPath = fileURLToPath(new URL(".", import.meta.url));
+  const fileRead = join(dirPath, "files", "fileToRead.txt");
+  const stream = createReadStream(fileRead, "utf-8");
+
+  stream.pipe(stdout);
 };
 
 await read();
